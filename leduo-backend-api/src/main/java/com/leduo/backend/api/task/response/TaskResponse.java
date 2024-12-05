6cf76a14da83b279b0e477fc69db3eac6152e265
leduoayng <leduoyang1995@gmail.com>
2024-12-05 00:04:15 -0800

    new: init backend-api for API definitions and request/response objects

---
+++ leduo-backend-api/src/main/java/com/leduo/backend/api/task/response/TaskResponse.java
@@ -0,0 +1,22 @@
+package com.leduo.backend.api.task.response;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Accessors(chain = true)
+@Data
+public class TaskResponse {
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private String status;
+
+    private Date created_at;
+
+    private Date updated_at;
+}
